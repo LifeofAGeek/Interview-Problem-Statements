f3f3020355f81cf3dd163bf2de8c1c9b398b720f
Anubhav Sethi <32430170+LifeofAGeek@users.noreply.github.com>
2020-04-16 17:06:28 +0530

    Valid Parenthesis String

--- ValidString.py
+++ ValidString.py
@@ -1,15 +1,16 @@
-str=')*('
-c=0
-h=0
-for i in range(0,len(str)):
-    if str[i]=='{':
-        c+=1
-    if str[i]=="}":
-        c-=1
-    if str[i]=='*':
-        h+=1
-c=c-h
-if c!=0:
-    print("Not Valid")
-else:
-    print("Valid")
+class Solution {
+public:
+    bool checkValidString(string s) {
+    int l = s.length() - 1;
+    int openCount = 0, closedCount = 0;
+    for (int i = 0; i <= l; i++)
+    {
+        if (s[i] == '*' || s[i] == '(') openCount++;
+        else openCount--;
+        if (s[l - i] == '*' || s[l - i] == ')') closedCount++;
+        else closedCount--;
+        if (openCount < 0 || closedCount < 0) return false;
+    }
+    return true;
+    }
+};
