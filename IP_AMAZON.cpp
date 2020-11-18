de44cb8a6ec33045e8942f100c5ca095f9757e04
Anubhav Sethi <32430170+LifeofAGeek@users.noreply.github.com>
2020-11-18 16:10:28 +0530

    This problem was recently asked in amazon.
    Question:
    Given an array of size n with unique positive integers and a positive integer K,
    check if there exists a combination of elements in the array satisfying below constraints.
    a. The sum of all such elements is K
    b. None of those elements are adjacent in the original array
    
    Ex:
    Input:
    arr = {1, 9, 8, 3, 6, 7, 5, 11, 12, 4}
    K = 14
    
    Output:
    [3, 7, 4]

---
+++ IP_AMAZON.cpp
@@ -0,0 +1,84 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+#define loop(i, start, end) for (int i = start; i <= end; i++)
+#define rloop(i, start, end) for (int i = start; i >= end; i--)
+#define vi vector<int>
+#define vec(x) vector<x>
+#define sz(v) (int)v.size() 
+#define UB upper_bound
+#define LB lower_bound
+#define all(v) (v).begin(),(v).end()
+#define arl(v) (v).rbegin(),(v).rend()
+#define fsp(a) fixed<<setprecision(a)
+#define mem(a,with) memset(a, with, sizeof (a))
+#define vmn(a) (*min_element(a.begin(),a.end()))   
+#define vmx(a) (*max_element(a.begin(),a.end()))
+#define bs(a,key) binary_search(all(a),key)    /// return bool.
+#define rotl(a,x) rotate(a.begin(),a.begin()+x,a.end());
+#define rotr(a,x) rotl(a,a.size()-x);
+#define nl cout<<endl
+#define dbg(x) cerr << #x << " :: " << x << endl;
+#define dbg2(x, y) cerr << #x << " :: " << x << "\t" << #y << " :: " << y << endl;
+#define MOD 1000000007
+int add(int x, int y) {int res = x + y; return (res >= MOD ? res - MOD : res);}
+int mul(int x, int y) {int res = x * y; return (res >= MOD ? res % MOD : res);}
+int power(int x, int y) {int res = 1; x %= MOD; while (y) {if (y & 1)res = mul(res, x); y >>= 1; x = mul(x, x);} return res;}
+
+vector<vi> ans;
+vi arr,temp;
+int n,k;
+
+void f(int idx,int sum){
+
+    if(sum==k){
+        ans.push_back(temp);
+        return;
+    }
+
+    if(idx>=n or sum>k) return;
+
+    f(idx+1,sum);
+    temp.push_back(arr[idx]);
+    f(idx+2,sum+arr[idx]);
+    temp.pop_back();
+}
+
+void solve(){
+    //code goes here
+    cin>>n>>k;
+    arr.resize(n);
+    loop(i,0,n-1) cin>>arr[i];
+
+    f(0,0);
+
+    if(ans.size()==0){
+        cout<<-1;
+        return;
+    }
+
+    for(auto it:ans){
+        for(auto x:it){
+            cout<<x<<" ";
+        }
+        nl;
+    }
+}
+
+int32_t main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+ios_base::sync_with_stdio(false);
+cout.tie(NULL);
+cin.tie(NULL);
+    int t=1;
+    // cin>>t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
